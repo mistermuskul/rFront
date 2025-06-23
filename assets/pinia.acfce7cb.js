dd9aa6d18b1b609670b43086a82d0c41198e1d3a
mistermuskul <mistermuskul@users.noreply.github.com>
2025-06-23 09:35:39 +0000

    deploy: c602359cc8d119d4ebc01aee615c99186f3d78e4

---
+++ assets/pinia.acfce7cb.js
@@ -0,0 +1,1 @@
+import{b as o}from"./index.78c4b5c8.js";import{c as r}from"./pinia.082cffec.js";var e=o(({app:a})=>{a.use(r())});export{e as default};
--- assets/pinia.d870b133.js
+++
@@ -1,1 +0,0 @@
-import{b as o}from"./index.bd04a65a.js";import{c as r}from"./pinia.d794cae7.js";var e=o(({app:a})=>{a.use(r())});export{e as default};
